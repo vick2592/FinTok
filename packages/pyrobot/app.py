c91f751b9e4fecd69088b6e398d456d2be9fc433
vick2592 <vick2592@yahoo.com>
2023-10-15 23:16:10 -0400

    PyRobot historic Data download and updater integrated

--- packages/pyrobot/app.py
+++ packages/pyrobot/app.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO
 from apscheduler.schedulers.background import BackgroundScheduler
 from routes import APIRoutes
 from cache import Cache
-from updater import start_updates_cle
+from updater import start_updates_cle, start_updates_bfe
 
 # Initialize app and other global resources
 app = Flask(__name__)
@@ -13,9 +13,13 @@ cache = Cache()
 # Initialize routes
 api_routes = APIRoutes(app, socketio, cache)
 
+#To get initial Chainlink Ethereum price
+start_updates_cle()
+
 # Initialize and start data updater
 scheduler = BackgroundScheduler()
 scheduler.add_job(start_updates_cle, 'interval', seconds=60)
+scheduler.add_job(start_updates_bfe, 'interval', seconds=60) # do 3600 every hour but for testing let it run every minute
 scheduler.start()
 
 if __name__ == "__main__":
--- packages/pyrobot/downloader.py
+++ packages/pyrobot/downloader.py
@@ -1,21 +1,36 @@
-import bitfinex
-import datetime
+import os
+from datetime import datetime, timedelta
 import time
 import pandas as pd
-import os
+import bitfinex
 
-def download_historical_data_to_csv(symbol, timeframe, limit, start_date, end_date):
+def download_data_to_csv(symbol, timeframe, limit):
+    print(f"Downloading {symbol} {timeframe} data...")
+    
+    # Define the start date
+    start_date = datetime(2018, 1, 1, 0, 0)
+    start_date = int(time.mktime(start_date.timetuple()) * 1000)
+
+    # Define the end date (current date and time)
+    end_date = int(time.time() * 1000)
+    # end_date = datetime(2020, 1, 1, 0, 0)
+    # end_date = int(time.mktime(end_date.timetuple()) * 1000)
+    #print(start_date, end_date)
+
     # Check if the CSV file exists and get the last datetime value
-    csv_file_name = f"{symbol}_{timeframe}_historical.csv"
+    csv_file_name = f"{symbol}_{timeframe}.csv"
     last_datetime = None
 
     if os.path.exists(csv_file_name):
-        existing_data = pd.read_csv(csv_file_name)
+        existing_data = pd.read_csv(csv_file_name, parse_dates=['Date'], date_parser=pd.to_datetime)
         if not existing_data.empty:
-            last_datetime = existing_data['MTS'].iloc[-1]
+            last_datetime = existing_data['Date'].iloc[-1]
 
     if last_datetime:
-        start_date = last_datetime.timestamp() * 1000
+        last_datetime = pd.to_datetime(last_datetime)
+        next_datetime = last_datetime + timedelta(hours=1)
+        last_timestamp = int(next_datetime.timestamp() * 1000)
+        start_date = last_timestamp
 
     # Create api instance
     api_v2 = bitfinex.bitfinex_v2.api_v2()
@@ -23,26 +38,45 @@ def download_historical_data_to_csv(symbol, timeframe, limit, start_date, end_da
     step = hour * limit
     data = []
 
+    #print(start_date, end_date)
+
     while start_date < end_date:
         if start_date + step > end_date:
             step = end_date - start_date
 
         end = start_date + step
         data += api_v2.candles(symbol=symbol, interval=timeframe, limit=limit, start=start_date, end=end)
-        print(f"Downloaded data from {start_date} to {end}")
-        start_date = end
-        time.sleep(1.5)
 
-    names = ['MTS', 'Open', 'Close', 'High', 'Low', 'Volume']
+        # Convert timestamps to datetime objects for printing and formatting
+        start_datetime = datetime.fromtimestamp(start_date / 1000)
+        end_datetime = datetime.fromtimestamp(end / 1000)
+        print(f"Downloaded data from {start_datetime} to {end_datetime}")
+
+        start_date = end
+        time.sleep(3)
+        
+    #print(data[:20])
+    names = ['Date', 'Open', 'Close', 'High', 'Low', 'Volume']
     df = pd.DataFrame(data, columns=names)
-    df['MTS'] = pd.to_datetime(df['MTS'], unit='ms')
-    df.set_index('MTS', inplace=True)
+    df.drop_duplicates(inplace=True)
+    df['Date'] = pd.to_datetime(df['Date'], unit='ms')
+    #df['Date'] = df['Date'].dt.strftime('%m-%d-%Y %H:%M')  # Format 'Date' as needed
+    df.set_index('Date', inplace=True)
     df.sort_index(inplace=True)
+    #print(str(df))
 
     # Append new data to the existing CSV file or create a new one if it doesn't exist
     if os.path.exists(csv_file_name):
         existing_data = pd.read_csv(csv_file_name)
+        existing_data.set_index('Date', inplace=True)
         updated_data = pd.concat([existing_data, df])
-        updated_data.to_csv(csv_file_name, index=False)
+        updated_data.to_csv(csv_file_name, index=True)  # Save the index along with the data
     else:
-        df.to_csv(csv_file_name, index=False)
+        df.to_csv(csv_file_name, index=True)  # If the file doesn't exist, save the index
+
+
+symbol = 'ETHUSD'  # Example: Ethereum/US Dollar
+timeframe = '1h'    # Fetch 1-hour candlestick data
+limit = 1000           # Example: Number of data points to retrieve
+
+download_data_to_csv(symbol, timeframe, limit)
